5f3a6a9363775c411246487a2f6c19f12ddb1b34
Radenz <radenrifqirahman@gmail.com>
2023-02-26 08:19:24 +0700

    cleanup: remove unused code

--- src/main.ts
+++ src/main.ts
@@ -10,22 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("canvas")! as HTMLCanvasElement;
   const viewer = new Viewer(canvas);
 
-  // const sq = viewer.createSquare(Transform.origin, 0.25);
-  // sq.vertices[1].color = Color.red;
-  // sq.vertices[2].color = Color.blue;
-  // sq.vertices[3].color = Color.green;
-  // sq.size = 0.25;
-
-  // const ln = viewer.createLine(Transform.origin, 0.4);
-  // const rect = viewer.createRectangle(Transform.origin, 0.6, 0.2);
-
-  // window["sq"] = sq;
-  // window["ln"] = ln;
-  // window["rect"] = rect;
-  window["canvas"] = canvas;
-  window["viewer"] = viewer;
-  window["Polygon"] = Polygon;
-
   setupButtons(viewer);
 
   const objectList = document.getElementById("object-list");
